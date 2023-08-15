ec45df6cea4ffb7fa545f7b1db2ba7eeb849f727
Hashiv07 <136419204+Hashiv07@users.noreply.github.com>
2023-08-15 01:03:24 -0400

    pelea_gallosmon.py

--- pelea_gallosmon.py
+++ pelea_gallosmon.py
@@ -1,18 +1,26 @@
 from funciones_lectura_escritura import leer_archivo_json
+GALLOSMON = leer_archivo_json("Gallomon\personajes.json")
 
 def obtener_atributos_gallosmon(gallo_id):
-    gallosmon = leer_archivo_json("Gallomon\personajes.json")
+    for gallomon in GALLOSMON:
+        if gallomon["id"] == gallo_id:
+            atributos = gallomon
     
-    atributos = gallosmon[gallo_id]
-    return atributos["nombre"], atributos["tipo"], atributos["base"]["ataque"], atributos["base"]["defensa"], atributos["base"]["vida"]
+    nombre = atributos["nombre"]
+    tipo = atributos["tipo"]
+    ataque = atributos["base"]["ataque"]
+    defensa = atributos["base"]["defensa"]
+    vida = atributos["base"]["vida"]
+    
+    return nombre, tipo, ataque, defensa, vida
         
 def pelea(gallosmon1, gallosmon2):
-    vida1, ataque1, defensa1 = gallosmon1
-    vida2, ataque2, defensa2 = gallosmon2
+    nombre1, tipo1, ataque1, defensa1, vida1 = gallosmon1
+    nombre2, tipo2, ataque2, defensa2, vida2 = gallosmon2
      
     vida1 -= max(0, ataque2 - defensa1)
     vida2 -= max(0, ataque1 - defensa2)
-    return [vida1, ataque1, defensa1], [vida2, ataque2, defensa2]
+    return [nombre1, tipo1, ataque1, defensa1, vida1], [nombre2, tipo2, ataque2, defensa2, vida2]
 
 
 #print(obtener_atributos_gallosmon(1))

146137a06874cc99cd8b59a7f9391846e26f1512
Hashiv07 <136419204+Hashiv07@users.noreply.github.com>
2023-08-18 00:30:46 -0400

    principal.py

--- principal.py
+++ principal.py
@@ -2,32 +2,32 @@ from pelea_gallosmon import GallosmonClase
 import time
 
 gallosmon = GallosmonClase()
-id_gallosmon1 = 1
-id_gallosmon2 = 2
-id_gallosmon3 = 3
-id_gallosmon4 = 4
-id_gallosmon5 = 5
-id_gallosmon6 = 6
-atributos_gallosmon1 = gallosmon.obtener_atributos_gallosmon(id_gallosmon1)
-atributos_gallosmon2 = gallosmon.obtener_atributos_gallosmon(id_gallosmon2)
-atributos_gallosmon3 = gallosmon.obtener_atributos_gallosmon(id_gallosmon3)
-atributos_gallosmon4 = gallosmon.obtener_atributos_gallosmon(id_gallosmon4)
-atributos_gallosmon5 = gallosmon.obtener_atributos_gallosmon(id_gallosmon5)
-atributos_gallosmon6 = gallosmon.obtener_atributos_gallosmon(id_gallosmon6)
+idg1 = 1     #id_gallosmon1
+idg2 = 2
+idg3 = 3
+idg4 = 4
+idg5 = 5
+idg6 = 6
+atributos_g1 = gallosmon.o_a_g(idg1)   #atributos_gallosmon1
+atributos_g2 = gallosmon.o_a_g(idg2)
+atributos_g3 = gallosmon.o_a_g(idg3)
+atributos_g4 = gallosmon.o_a_g(idg4)
+atributos_g5 = gallosmon.o_a_g(idg5)
+atributos_g6 = gallosmon.o_a_g(idg6)
 
-resultado_pelea = gallosmon.pelea(atributos_gallosmon1, atributos_gallosmon2)
+resultado_pelea = gallosmon.pelea(atributos_g1, atributos_g2)
 
 print("\n¡Bienvenido al Mundo Gallosmon!")
 time.sleep(3)
 
 print("\n¿Cuál Gallosmon eliges?")
 
-print("\n1- ", atributos_gallosmon1[0])
-print("2- ", atributos_gallosmon2[0])
-print("3- ", atributos_gallosmon3[0])
-print("4- ", atributos_gallosmon4[0])
-print("5- ", atributos_gallosmon5[0])
-print("6- ", atributos_gallosmon6[0])
+print("\n1- ", atributos_g1[0])
+print("2- ", atributos_g2[0])
+print("3- ", atributos_g3[0])
+print("4- ", atributos_g4[0])
+print("5- ", atributos_g5[0])
+print("6- ", atributos_g6[0])
 
 
 eleccion = input("\nElija un número: ")
@@ -35,66 +35,67 @@ eleccion = input("\nElija un número: ")
 try:
         eleccion = int(eleccion)
         if eleccion == "1":
-            gallosmon_elegido = atributos_gallosmon1
+            gallosmon_e = atributos_g1    #gallosmon_elegido
         elif eleccion == "2":
-            gallosmon_elegido = atributos_gallosmon2
+            gallosmon_e = atributos_g2
         elif eleccion == "3":
-            gallosmon_elegido = atributos_gallosmon3
+            gallosmon_e = atributos_g3
         elif eleccion == "4":
-            gallosmon_elegido = atributos_gallosmon4
+            gallosmon_e = atributos_g4
         elif eleccion == "5":
-            gallosmon_elegido = atributos_gallosmon5
+            gallosmon_e = atributos_g5
         elif eleccion == "6":
-            gallosmon_elegido = atributos_gallosmon6
+            gallosmon_e = atributos_g6
         else:
             print("Opción inválida. Se usará el Gallosmon por defecto.")
-            gallosmon_elegido = atributos_gallosmon1
+            gallosmon_e = atributos_g1
 
         time.sleep(3)
     
 except ValueError:
         print("Opción inválida. Se usará el Gallosmon por defecto.")
-        gallosmon_elegido = atributos_gallosmon1
+        gallosmon_e = atributos_g1
         time.sleep(3)
 
 
 print("\n-------BATALLA GALLOSMON--------")
-print(F"\nNOMBRE/", gallosmon_elegido[0])
-print("VIDA/", gallosmon_elegido[4])
-print("TIPO/", gallosmon_elegido[1])
-print("ATAQUE/", gallosmon_elegido[2])
-print("DEFENSA/", gallosmon_elegido[3])
+print(F"\nNOMBRE/", gallosmon_e[0])
+print("VIDA/", gallosmon_e[4])
+print("TIPO/", gallosmon_e[1])
+print("ATAQUE/", gallosmon_e[2])
+print("DEFENSA/", gallosmon_e[3])
 print("\nVS")
-print(F"\nNOMBRE/", atributos_gallosmon2[0])
-print("VIDA/", atributos_gallosmon2[4])
-print("TIPO/", atributos_gallosmon2[1])
-print("ATAQUE/", atributos_gallosmon2[2])
-print("DEFENSA/", atributos_gallosmon2[3])
+print(F"\nNOMBRE/", atributos_g2[0])
+print("VIDA/", atributos_g2[4])
+print("TIPO/", atributos_g2[1])
+print("ATAQUE/", atributos_g2[2])
+print("DEFENSA/", atributos_g2[3])
 
 time.sleep(3)
 
 print("\nHA INICIADO LA BATALLA")
-print(f"\n{gallosmon_elegido[0]} se enfreta a {atributos_gallosmon2[0]} ...")
+print(f"\n{gallosmon_e[0]} se enfreta a {atributos_g2[0]} ...")
 time.sleep(5)
 
-print(f"\n{gallosmon_elegido[0]} ataca a {atributos_gallosmon2[0]} ...")
+print(f"\n{gallosmon_e[0]} ataca a {atributos_g2[0]} ...")
 time.sleep(5)
 
 print(f"\nEl ataque es muy efectivo !!!...")
 time.sleep(5)
 
-print(f"\n{atributos_gallosmon2[0]} ataca a {gallosmon_elegido[0]} ...")
+print(f"\n{atributos_g2[0]} ataca a {gallosmon_e[0]} ...")
 time.sleep(5)
 
 print(f"\nEl ataque no es muy efectivo !!!...")
 
 time.sleep(5)
 
-if gallosmon_elegido[4] <= 0:
-    print(f"\nGANO {atributos_gallosmon2[0]} !!!")
+if gallosmon_e[4] <= 0:
+    print(f"\nGANO {atributos_g2[0]} !!!")
 else:  
-    print(f"\nGANO {gallosmon_elegido[0]} !!!")
+    print(f"\nGANO {gallosmon_e[0]} !!!")
 
 time.sleep(2)
 
 print("El oponente te pagó $500")
+
